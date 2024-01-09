c70dc225766bc4dd40c9eef9df70129620e43ced
Tobias Aagaard <Tobias909@live.dk>
2024-01-09 10:17:01 +0100

    Lidt lier

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-let operation: string | "add" | "subtract" | "multiply" | "divide";
-
-function calculate(a: number, b: number, operation: number | string) {
+var operation;
+function calculate(a, b, operation) {
     switch (operation) {
         case "add":
             return a + b;
@@ -11,12 +10,11 @@ function calculate(a: number, b: number, operation: number | string) {
         case "divide":
             if (b !== 0) {
                 return a / b;
-            } else {
-                return "DON*T YOU F#!KING DARE DEVIDEING BY 0!!!"
+            }
+            else {
+                return "DON*T YOU F#!KING DARE DEVIDEING BY 0!!!";
             }
     }
 }
-
-
-const result = calculate(7, 5, "devide" )
+var result = calculate(7, 5, "devide");
 console.log(result);
